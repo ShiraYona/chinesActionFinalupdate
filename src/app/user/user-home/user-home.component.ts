b9cbff57cd16c6e6c9151b65311cdbcfb6794cfb
The user <The user@DESKTOP-E0FAPSB>
2023-12-20 02:33:39 +0200

    Add project files.

---
+++ src/app/user/user-home/user-home.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnChanges, OnInit ,EventEmitter,Output, Input} from '@angular/core';
+
+import { MessageService } from 'primeng/api';
+import { ConfirmationService } from 'primeng/api';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-user-home',
+  templateUrl: './user-home.component.html',
+  styleUrls: ['./user-home.component.css'],
+  providers: [ConfirmationService, MessageService]//זה לא אמור להיות קשור 
+
+})
+export class UserHomeComponent implements  OnInit {
+  constructor(
+ private activatedroute: ActivatedRoute,private router:Router
+) { }
+
+// @Input()
+// count:number=0
+// @Output()
+// CountChanged:EventEmitter<number>=new EventEmitter<number>()
+  
+    count:number=0
+  ngOnInit(): void {
+    if (sessionStorage.getItem("count")) {
+      this.count = JSON.parse(sessionStorage.getItem("count") ?? "0");
+    }
+  
+
+    setInterval(() => {
+      this.count = JSON.parse(sessionStorage.getItem('count')??"0"); // מתעדכן בכל רגע ע"י קריאה לערך מאגר הסשן
+    }, 100);
+  }
+
+  
+  
+  goToCart(){
+    
+    this.router.navigate(["./cart"],{relativeTo:this.activatedroute});
+  }
+
+managerDialog: boolean = false;
+  password:string="";
+  userName:string="";
+  flag:boolean=false;
+  submitted: boolean = false;
+
+goToManager(){
+
+  //this.managerDialog = true;
+  if(this.userName=="manager" && this.password=="manager")
+    this.router.navigate(["../manager/manager"],{relativeTo:this.activatedroute});
+  else{
+    alert("wrong userName or password, please try again");
+  }
+   this.hideDialog()
+}
+hideDialog() {
+  this.managerDialog = false;
+  // this.userDialogChange.emit(this.userDialog);
+  this.submitted = false;
+}
+
+
+}
